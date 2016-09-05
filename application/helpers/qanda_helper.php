55de456a1e507085809fed450f606a9ccfa29919
Olle Harstedt <olleharstedt@yahoo.com>
2016-09-05 21:02:43 +0200

    Dev: Factor out getAnswerAndInputNames()

--- application/helpers/qanda_helper.php
+++ application/helpers/qanda_helper.php
@@ -119,6 +119,124 @@ function retrieveAnswers($ia)
 
     $oQuestion = Question::model()->findByPk(array('qid'=>$ia[0], 'language'=>$lang));
 
+    //$answer is the html code to be printed
+    //$inputnames is an array containing the names of each input field
+    list($answer, $inputnames) = getAnswerAndInputNames($ia);
+
+    //If this question is mandatory but wasn't answered in the last page
+    //add a message HIGHLIGHTING the question
+    $mandatory_msg = getMandatoryMessage($ia);
+    $qtitle .= $mandatory_msg;
+
+    $_vshow = !isset($aQuestionAttributes['hide_tip']) || $aQuestionAttributes['hide_tip'] == 0; // whether should initially be visible - TODO should also depend upon 'hidetip'?
+
+    list($validation_msg, $isValid) = validation_message($ia,$_vshow);
+
+    $qtitle .= $validation_msg;
+
+    list($file_valid_message, $isValid) = getFileValidationMessage($ia);
+
+    $qtitle .= $ia[4] == "|" ? $file_validation_msg : "";
+
+    // =====================================================
+    // START: legacy question_start.pstpl code
+    // The following section adds to the templating system by allowing
+    // templaters to control where the various parts of the question text
+    // are put.
+
+    $sTemplate = isset($thissurvey['template']) ? $thissurvey['template'] : NULL;
+    if (is_file('templates/'.$sTemplate.'/question_start.pstpl'))
+    {
+        $qtitle_custom = '';
+
+        $replace=array();
+        foreach ($question_text as $key => $value)
+        {
+            $find[] = '{QUESTION_'.strtoupper($key).'}'; // Match key words from template
+            $replace[] = $value; // substitue text
+        };
+
+        if (!defined('QUESTION_START'))
+        {
+            define('QUESTION_START' , file_get_contents(getTemplatePath($thissurvey['template']).'/question_start.pstpl' , true));
+        };
+
+        $qtitle_custom = str_replace( $find , $replace , QUESTION_START);
+
+        $c = 1;
+        // START: <EMBED> work-around step 1
+        $qtitle_custom = preg_replace( '/(<embed[^>]+>)(<\/embed>)/i' , '\1NOT_EMPTY\2' , $qtitle_custom );
+        // END <EMBED> work-around step 1
+        while ($c > 0) // This recursively strips any empty tags to minimise rendering bugs.
+        {
+            $oldtitle=$qtitle_custom;
+            $qtitle_custom = preg_replace( '/<([^ >]+)[^>]*>[\r\n\t ]*<\/\1>[\r\n\t ]*/isU' , '' , $qtitle_custom , -1); // I removed the $count param because it is PHP 5.1 only.
+
+            $c = ($qtitle_custom!=$oldtitle)?1:0;
+        };
+        // START <EMBED> work-around step 2
+        $qtitle_custom = preg_replace( '/(<embed[^>]+>)NOT_EMPTY(<\/embed>)/i' , '\1\2' , $qtitle_custom );
+        // END <EMBED> work-around step 2
+        while ($c > 0) // This recursively strips any empty tags to minimise rendering bugs.
+        {
+            $oldtitle=$qtitle_custom;
+            $qtitle_custom = preg_replace( '/(<br(?: ?\/)?>(?:&nbsp;|\r\n|\n\r|\r|\n| )*)+$/i' , '' , $qtitle_custom , -1 ); // I removed the $count param because it is PHP 5.1 only.
+            $c = ($qtitle_custom!=$oldtitle)?1:0;
+        };
+
+        $question_text['all'] = $qtitle_custom;
+    }
+    else
+    {
+        $question_text['all'] = $qtitle;
+    };
+    // END: legacy question_start.pstpl code
+    //===================================================================
+    //
+
+    $qanda=array($question_text, $answer, 'help', $display, $qid, $ia[2], $ia[5], $ia[1] );
+
+    //New Return
+    /*
+    return array(
+      array(
+        array(
+          'all' => 'all', // deprecated?
+          'text' => 'text',
+          'number' => 1,
+          'input_error_class' => 'input_error_class',
+          'help' => 'help',
+          'valid_message' => 'valid_message',
+          'man_message' => 'man_message',
+          //'mandatory' => 'mandatory',
+          'file_valid_message' => 'file_valid_message',
+          'code' => 'code' // $ia[2]
+        ),
+        'answer',
+        'help',
+        'display',
+        $ia[0], // 'qid',
+        'ia[2]',
+        'ia[5]',
+        '123X123X123'
+      ),
+      array(
+        '123X123X123'
+      )
+    );
+     */
+
+    return array($qanda, $inputnames);
+}
+
+/**
+ * Get "values", meaning answer HTML and input codes
+ * @param array $ia
+ * @return array
+ */
+function getAnswerAndInputNames($ia)
+{
+    $values = null;
     switch ($ia[4])
     {
         case 'X': //BOILERPLATE QUESTION
@@ -300,116 +418,7 @@ function retrieveAnswers($ia)
         default:
             throw new Exception("Unknown question type: " . $ia[4]);
     }
-
-    //$answer is the html code to be printed
-    //$inputnames is an array containing the names of each input field
-    list($answer, $inputnames)=$values;
-
-    //If this question is mandatory but wasn't answered in the last page
-    //add a message HIGHLIGHTING the question
-    $mandatory_msg = getMandatoryMessage($ia);
-    $qtitle .= $mandatory_msg;
-
-    $_vshow = (!isset($aQuestionAttributes['hide_tip']) || $aQuestionAttributes['hide_tip']==0)?true:false; // whether should initially be visible - TODO should also depend upon 'hidetip'?
-
-    list($validation_msg, $isValid) = validation_message($ia,$_vshow);
-
-    $qtitle .= $validation_msg;
-
-    list($file_valid_message, $isValid) = getFileValidationMessage($ia);
-
-    $qtitle .= $ia[4] == "|" ? $file_validation_msg : "";
-
-    // =====================================================
-    // START: legacy question_start.pstpl code
-    // The following section adds to the templating system by allowing
-    // templaters to control where the various parts of the question text
-    // are put.
-
-    $sTemplate = isset($thissurvey['template']) ? $thissurvey['template'] : NULL;
-    if (is_file('templates/'.$sTemplate.'/question_start.pstpl'))
-    {
-        $qtitle_custom = '';
-
-        $replace=array();
-        foreach ($question_text as $key => $value)
-        {
-            $find[] = '{QUESTION_'.strtoupper($key).'}'; // Match key words from template
-            $replace[] = $value; // substitue text
-        };
-
-        if (!defined('QUESTION_START'))
-        {
-            define('QUESTION_START' , file_get_contents(getTemplatePath($thissurvey['template']).'/question_start.pstpl' , true));
-        };
-
-        $qtitle_custom = str_replace( $find , $replace , QUESTION_START);
-
-        $c = 1;
-        // START: <EMBED> work-around step 1
-        $qtitle_custom = preg_replace( '/(<embed[^>]+>)(<\/embed>)/i' , '\1NOT_EMPTY\2' , $qtitle_custom );
-        // END <EMBED> work-around step 1
-        while ($c > 0) // This recursively strips any empty tags to minimise rendering bugs.
-        {
-            $oldtitle=$qtitle_custom;
-            $qtitle_custom = preg_replace( '/<([^ >]+)[^>]*>[\r\n\t ]*<\/\1>[\r\n\t ]*/isU' , '' , $qtitle_custom , -1); // I removed the $count param because it is PHP 5.1 only.
-
-            $c = ($qtitle_custom!=$oldtitle)?1:0;
-        };
-        // START <EMBED> work-around step 2
-        $qtitle_custom = preg_replace( '/(<embed[^>]+>)NOT_EMPTY(<\/embed>)/i' , '\1\2' , $qtitle_custom );
-        // END <EMBED> work-around step 2
-        while ($c > 0) // This recursively strips any empty tags to minimise rendering bugs.
-        {
-            $oldtitle=$qtitle_custom;
-            $qtitle_custom = preg_replace( '/(<br(?: ?\/)?>(?:&nbsp;|\r\n|\n\r|\r|\n| )*)+$/i' , '' , $qtitle_custom , -1 ); // I removed the $count param because it is PHP 5.1 only.
-            $c = ($qtitle_custom!=$oldtitle)?1:0;
-        };
-
-        $question_text['all'] = $qtitle_custom;
-    }
-    else
-    {
-        $question_text['all'] = $qtitle;
-    };
-    // END: legacy question_start.pstpl code
-    //===================================================================
-    //
-    //$question_text['all'] = 'dummy';
-    
-    $qanda=array($question_text, $answer, 'help', $display, $qid, $ia[2], $ia[5], $ia[1] );
-
-    //New Return
-    /*
-    return array(
-      array(
-        array(
-          'all' => 'all', // deprecated?
-          'text' => 'text',
-          'number' => 1,
-          'input_error_class' => 'input_error_class',
-          'help' => 'help',
-          'valid_message' => 'valid_message',
-          'man_message' => 'man_message',
-          //'mandatory' => 'mandatory',
-          'file_valid_message' => 'file_valid_message',
-          'code' => 'code' // $ia[2]
-        ),
-        'answer',
-        'help',
-        'display',
-        $ia[0], // 'qid',
-        'ia[2]',
-        'ia[5]',
-        '123X123X123'
-      ),
-      array(
-        '123X123X123'
-      )
-    );
-     */
-
-    return array($qanda, $inputnames);
+    return $values;
 }
 
 /**
